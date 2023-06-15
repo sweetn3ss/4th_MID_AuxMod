6e54b8dda1bcaffd8e206b5f88773244b65d3012
Daisy <92063434+Anderanged@users.noreply.github.com>
2023-06-15 03:35:21 -0700

    base classes created - not up to date with CfgAmmo

--- 4th_airVics/4th_weapons.hpp
+++ 4th_airVics/4th_weapons.hpp
@@ -1,3 +1,21 @@
 class CfgWeapons {
-
+    //hornet
+    class 4th_127x99_rotaryCannon
+    {};
+    //sideguns of falcon, reargun of pelican
+    class 4th_127x99_mGun
+    {};
+    //falcon chingun
+    class 4th_25mm_cannon
+    {};
+    //pelican chingun
+    class 4th_70mm_cannon
+    {};
+    //missles
+    class 4th_ANVIL2_launcher
+    {};
+    class 4th_ASGM4_launcher
+    {};
+    class 4th_Scorpion_launcher
+    {};
 };
