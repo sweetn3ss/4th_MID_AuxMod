a5184a6210b27063018e66af53249918ae0d2fdc
Daisy <92063434+Anderanged@users.noreply.github.com>
2023-06-15 03:34:55 -0700

    reformatted and futureproofed

--- 4th_airVics/config.cpp
+++ 4th_airVics/config.cpp
@@ -2,14 +2,20 @@
 
 class CfgPatches
 {
-	class 4th_airVics
+	class 4th_UNSC_aircraft
 	{
-		units[] = {};
+		units[] = {"4th_hornet","4th_falcon","4th_falcon_u","4th_falcon_s","4th_falcon_u_s","4th_pelican"};
 		weapons[] = {};
-		requiredVersion = 0.1;
-		requiredAddons[] = {"4th_Armour","A3_Weapons_F"};
-		author = "Daisy";
 		magazines[] = {};
 		ammo[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {}; 
+		/*
+		CfgAmmo req Addons: 		["A3_Sounds_F","A3_Weapons_F_Jets","A3_Weapons_F","ace_rearm","ace_frag","ace_ballistics","ace_medical_damage","ace_vehicle_damage","ace_hellfire"]
+		CfgMagazines req Addons:	
+		CfgWeapons req Addons:		
+		CfgVehicles req Addons:			
+		*/
+		author = "Daisy";
 	};
 };
